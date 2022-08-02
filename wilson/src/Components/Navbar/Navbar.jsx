172ffbf6bd89af24ea9c3c2742a91edeca46877f
Keanu Barnard <keanu.barnardd@gmail.com>
2022-08-02 23:47:31 +1000

    Mobile nav styling
    Responsive navbar complete.

--- wilson/src/Components/Navbar/Navbar.jsx
+++ wilson/src/Components/Navbar/Navbar.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import {AiFillHome,AiFillPhone,AiFillFileText,AiFillQuestionCircle} from "react-icons/ai";
+
 
 import "./Navbar.scss";
 import "../../App.scss";
@@ -12,15 +14,19 @@ const Navbar = () => {
         <div className="navbar__links">
           <NavLink to="/" className={({ isActive }) => (isActive ? "active navlinks" : "navlinks")}>
             Home
+            <AiFillHome className="navbar__navlinks-icon"/>
           </NavLink>
           <NavLink to="/about" className={({ isActive }) => (isActive ? "active navlinks" : "navlinks")}>
             About
+            <AiFillQuestionCircle className="navbar__navlinks-icon"/>
           </NavLink>
           <NavLink to="/careers" className={({ isActive }) => (isActive ? "active navlinks" : "navlinks")}>
             Careers
+            <AiFillFileText className="navbar__navlinks-icon"/>
           </NavLink>
           <NavLink to="/contact" className={({ isActive }) => (isActive ? "active navlinks" : "navlinks")}>
             Contact
+            <AiFillPhone className="navbar__navlinks-icon"/>
           </NavLink>
         </div>
       </ul>

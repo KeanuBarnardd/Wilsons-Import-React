a3c81de6db71cea48d4f5f8a1a779edd6faf27d0
Keanu Barnard <keanu.barnardd@gmail.com>
2022-08-03 12:31:46 +1000

    Header Styling + Mobile
    Completed header styling + Mobile scope

--- wilson/src/Containers/Header/Header.jsx
+++ wilson/src/Containers/Header/Header.jsx
@@ -1,26 +1,42 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
 import "./Header.scss";
 import { images } from "../../Constants";
 
 const Header = ({ backgroundImg }) => {
   return (
-    <header className="app__flex" style={{backgroundImage: `url(${backgroundImg})`}}>
-      <div className="header__content app__container-width">
-        <h1 className="header__title">Your ultimate trucking and warehousing services.</h1>
-        <p className="header__text">
+    <header className="app__flex" style={{ backgroundImage: `url(${backgroundImg})` }}>
+      <motion.div className="header__content app__container-width">
+        <motion.h1
+          className="header__title"
+          whileInView={{ x: [-200, 0], opacity: [0, 1] }}
+          transition={{ duration: 0.8 }}
+        >
+          Your ultimate trucking and warehousing services.
+        </motion.h1>
+
+        <motion.p
+          className="header__text"
+          whileInView={{ x: [-200, 0], opacity: [0, 1] }}
+          transition={{ duration: 1.5 }}
+        >
           Here at wilsonport we pride ourselves in being known for our quality services. Your
           satisfaction and happiness is our number one priority all the time.
-        </p>
-        <div className="header__buttons-container">
-          <button className="header__button">
+        </motion.p>
+        <motion.div
+          className="header__buttons-container"
+          whileInView={{ x: [-200, 0], opacity: [0, 1] }}
+          transition={{ duration: 1.9 }}
+        >
+          <button className="header__button header__button-orange">
             <Link to="/contact">Get in Touch</Link>
           </button>
-          <button className="header__button">
+          <button className="header__button header__button-clear">
             <Link to="/about">About us</Link>
           </button>
-        </div>
-      </div>
+        </motion.div>
+      </motion.div>
     </header>
   );
 };

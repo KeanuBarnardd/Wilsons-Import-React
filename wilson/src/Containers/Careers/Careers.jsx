c9db59855621ef913096ec1367ae4090399327b4
Keanu Barnard <keanu.barnardd@gmail.com>
2022-08-04 14:22:02 +1000

    Careers header added
    Styling

--- wilson/src/Containers/Careers/Careers.jsx
+++ wilson/src/Containers/Careers/Careers.jsx
@@ -1,11 +1,30 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import { images } from "../../Constants";
+import { motion } from "framer-motion";
 
-import './Careers.scss';
+import "./Careers.scss";
 
 const Careers = () => {
   return (
-    <div>Careers</div>
-  )
-}
+    <div
+      className="app__flex careers__header"
+      style={{ backgroundImage: `url(${images.trucksBirdsEyeview})` }}
+    >
+      <div className="careers__header-content app__container-width">
+        <motion.h1 whileInView={{ x: [-50, 0], opacity: [0, 1] }}>
+          Feel like your ready to be apart of our team?
+        </motion.h1>
+        <motion.button
+          className="careers__header-button"
+          to={"/contact"}
+          whileInView={{ x: [-50, 0], opacity: [0, 1] }}
+        >
+          <Link to={"/contact"} className="careers__link">Join our team</Link>
+        </motion.button>
+      </div>
+    </div>
+  );
+};
 
-export default Careers
+export default Careers;
